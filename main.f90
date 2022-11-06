af41bb9aec7882f38997242611286db470dffae0
Sergio Guerra <alu0101133201@ull.edu.es>
2022-11-06 21:06:44 +0000

    Creating tree and inserting godNames. Not destroying the tree yet

--- main.f90
+++ main.f90
@@ -1,17 +1,36 @@
 program mainProg
+    use :: bst_mod 
     implicit none
-    character(len=15) :: firstName ! esto tiene que ser de longitud variable
-    character(len=15) :: secondName
-    character(len=15) :: transaction
-    character(len=15) :: preposition
-    real :: amount
+    ! Initial length for reading data, will be trimed for storage
+    integer,        parameter :: upperBoundLength = 20
     integer :: ios
+    ! Variables needed for processing input lines
+    character(len=upperBoundLength) :: firstName 
+    character(len=upperBoundLength) :: secondName
+    character(len=upperBoundLength) :: transaction
+    character(len=upperBoundLength) :: preposition
+    real :: amount
+    ! Binary search tree declaration
+    type(a_tree_node), pointer :: root 
+    root => null()
 
     do
         read (*, *, iostat=ios) firstName, transaction, amount, preposition, secondName
         if (ios < 0) exit 
-        print *, firstName, transaction, amount, preposition, secondName
-       
+        ! IMPORTANT - These variables need to be trimmed (trim(firstName)...)
+        ! when passing them to the tree
+        
+        if ((transaction == "lent") .or. (transaction == "borrowed")) then
+            block
+                character(len=:), allocatable :: firstGodName 
+                character(len=:), allocatable :: secondGodName 
+                firstGodName = trim(firstName)
+                secondGodName = trim(secondName)
+                call bstInsert(root, firstGodName)
+                call bstInsert(root, secondGodName)
+            end block
+        else
+            print *, "Wrong transaction '", trim(transaction), "'"
+        end if
     end do
-
 end program mainProg
